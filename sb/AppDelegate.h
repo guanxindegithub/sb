c656f308004e9fb26712d44ea6ba5ad75b8fde1d
guanxindegithub <tarena160102@wangqian-2.local>
2016-07-02 14:51:20 +0800

    2016 07 02

---
+++ sb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sb
+//
+//  Created by tarena160102 on 16/7/2.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sb
+//
+//  Created by tarena160102 on 16/7/2.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
